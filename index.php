355461dec3a314211dcebc48ef7cf232c703ffa2
takeaimnfire <takeaimnfire@comcast.net>
2020-05-01 09:05:03 -0400

    index edits

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
-<?PHP
+<?php
 
-    echo "Hello World! Im here!";
+    $firstname = "Dave";
 
+    echo "Hello $firstname!";
+
+    $number1 = 3;
+    $number2 = 4;
+
+    echo $number1 ** $number2;
+
+    $i = 0;
+
+    while ($i < 10) {
+        echo "Your number is $i<br/>";
+        $i++;
+    }
+
+    echo "<br/><br/>";
+
+    for ($k = 0; $k < 10; $k++) {
+        echo "Your number is $k<br/>";
+    }
+
+    $x = 0;
+
+    while ($x <= 100) {
+        if ($x % 3 == 0 && $x % 5 == 0) {
+            echo "$x: FIZZBUZZ!<br/>";
+        } elseif ($x % 3 == 0) {
+            echo "$x: FIZZ!<br/>";
+        } elseif ($x % 5 == 0) {
+            echo "$x: BUZZ!<br/>";
+        } else {
+            echo "$x<br/>";
+        }
+        $x++;
+    }
+
+    $pizza_toppings = array("Pepperoni", "Cheese", "Mushrooms");
+
+    echo $pizza_toppings[0] . "<br/>";
+
+    $l = 0;
+
+    while ($l <= 2) {
+        echo $pizza_toppings[$l] . "<br/>";
+        $l++;
+    }
+
+    $age = array("Mary"=>"24", "Luke"=>"38", "Dave"=>"31");
+
+    echo $age["Dave"];
 ?>
