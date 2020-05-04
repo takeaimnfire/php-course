5133c7a71264324f9c245583a680b3a87b8377f8
takeaimnfire <takeaimnfire@comcast.net>
2020-05-04 13:44:47 -0400

    more css and logic changes

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <div style="position: relative; text-align: center; margin-top: 0px;" id="header">
         <a href="addition.php">Addition</a> | <a href="subtraction.php">Subtraction</a> | <a href="multiply.php">Multiplication</a> | <a href="divide.php">Division</a>
         <br/><br/>
+        <a href="index.php">Go Back To Index</a>
 </div>
