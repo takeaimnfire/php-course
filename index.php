53e7b8d8f41000a90d3af57dc27a5b2cc572cda5
takeaimnfire <takeaimnfire@comcast.net>
2020-05-05 10:20:35 -0400

    added head tags

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PHP Testing</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
--- subtraction.php
+++ subtraction.php
@@ -1,5 +1,9 @@
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>Math Flashcards</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
